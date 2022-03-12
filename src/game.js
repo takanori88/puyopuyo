a60271653c555e599cc3ebc9d7b12121695fd57f
takanori88 <takanori.kawakami@flowing-vision.com>
2022-03-13 06:08:16 +0900

    game.js finished

--- src/game.js
+++ src/game.js
@@ -17,3 +17,79 @@ function initialize() {
   mode = 'start';
   frame = 0;
 }
+
+function loop() {
+  switch (mode) {
+    case "start":
+      mode = "checkFall"
+      break
+    case "checkFall":
+      if (Stage.checkFall()) {
+        mode = "fall"
+      } else {
+        mode = "checkErase"
+      }
+      break
+    case "fall":
+      if (!Stage.fall())  {
+        mode = "checkErase"
+      }
+      break
+    case "checkErase":
+      const eraseInfo = Stage.checkErase(frame)
+      if (eraseInfo) {
+        mode = "erasing"
+        combinationCount++
+        Score.calculateScore(combinationCount, eraseInfo.piece, eraseInfo.color)
+        Stage.hideZenkeshi()
+      } else {
+        if (Stage.puyoCount === 0 && combinationCount > 0) {
+          Stage.showZenkeshi()
+          Score.addScore(3600)
+        }
+        combinationCount = 0
+        mode = "newPuyo"
+      }
+      break
+    case "erasing":
+      if (!Stage.erasing(frame)) {
+        mode = "checkFall"
+      }
+      break
+    case "newPuyo":
+      if (!Player.createNewPuyo()) {
+        mode = "gameOver"
+      } else {
+        mode = "playing"
+      }
+      break
+    case "playing":
+      const action = Player.playing(frame)
+      mode = "action"
+      break
+    case "moving":
+      if (!Player.moving(frame)) {
+        mode = "playing"
+      }
+      break
+    case "rotating":
+      if (!Player.rotating(frame)) {
+        mode = "playing"
+      }
+      break
+    case "fix":
+      Player.fix()
+      mode = "checkFall"
+      break
+    case "gameOver":
+      PuyoImage.prepareBatankyu(frame)
+      mode = "batankyu"
+      break
+    case "batankyu":
+      PuyoImage.batankyu(frame)
+      Player.batankyu()
+      break
+  }
+  frame++
+  requestAnimationFrame(loop)
+}
